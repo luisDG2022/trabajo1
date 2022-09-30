f3cdd3dab6248ef1a4d88de8944e959d2c44c9d5
luis alberto Domínguez Daga <Luis.2000Dominguez@outlook.es>
2022-09-30 14:02:14 -0400

    me falta todo

--- trabajo1/Form1.Designer.cs
+++ trabajo1/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -60,11 +61,21 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "segundo apellido";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(849, 705);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 58);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1506, 891);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -80,5 +91,6 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private Button button1;
     }
 }
