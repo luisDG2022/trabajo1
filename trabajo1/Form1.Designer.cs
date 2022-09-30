69f490fa21d1149e259c993235f15030b9f21243
LUIS ALBERTO <LUIS ALBERTO@LAPTOP-E4P2MLCP>
2022-09-30 13:59:58 -0400

    Agregar archivos de proyecto.

---
+++ trabajo1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace trabajo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "primer nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(723, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "segundo nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 41);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "segundo apellido";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1506, 891);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
